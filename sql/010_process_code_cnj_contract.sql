f4303ca19f69c44c46968c031d727526503fc5fb
oigorbrito <oigorbrito@gmail.com>
2026-09-14 11:37:15 -0300

    refactor: make CNJ constraint migration idempotent

--- sql/010_process_code_cnj_contract.sql
+++ sql/010_process_code_cnj_contract.sql
@@ -1,5 +1,13 @@
-ALTER TABLE processes
-    ADD CONSTRAINT processes_code_canonical_cnj_chk
-    CHECK (
-        code ~ '^[0-9]{7}-[0-9]{2}\.[0-9]{4}\.[0-9]\.[0-9]{2}\.[0-9]{4}$'
-    ) NOT VALID;
+DO $$
+BEGIN
+    ALTER TABLE processes
+        ADD CONSTRAINT processes_code_canonical_cnj_chk
+        CHECK (
+            code ~ '^[0-9]{7}-[0-9]{2}\.[0-9]{4}\.[0-9]\.[0-9]{2}\.[0-9]{4}$'
+        ) NOT VALID;
+EXCEPTION
+    WHEN duplicate_object THEN NULL;
+END $$;
+
+COMMENT ON CONSTRAINT processes_code_canonical_cnj_chk ON processes IS
+    'New/updated process rows must use canonical CNJ formatting. NOT VALID avoids a deployment-time scan of historical rows.';
