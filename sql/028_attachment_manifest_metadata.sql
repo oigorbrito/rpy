2d8ddc9cfcd5b787639bcf0e7d893f4872408851
oigorbrito <oigorbrito@gmail.com>
2026-09-18 01:33:05 -0300

    db: add attachment source manifest metadata

---
+++ sql/028_attachment_manifest_metadata.sql
@@ -0,0 +1,7 @@
+ALTER TABLE process_attachments
+    ADD COLUMN IF NOT EXISTS source_name TEXT,
+    ADD COLUMN IF NOT EXISTS source_date TIMESTAMPTZ,
+    ADD COLUMN IF NOT EXISTS provider_status TEXT;
+
+CREATE INDEX IF NOT EXISTS idx_process_attachments_source_identity
+    ON process_attachments (process_id, version_id, source_attachment_id);
